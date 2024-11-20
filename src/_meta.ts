eddd90648ebb458c52c17550ec71fb8e35baa270
Kovah <mail@kovah.de>
2024-11-20 20:08:06 +0100

    Minor fixes, adjust site meta for SEO

---
+++ src/_meta.ts
@@ -0,0 +1,11 @@
+export const siteUrl = "https://bluesky-starter-packs.com"
+export const siteTitle = "Bluesky Starter Packs"
+export const siteDescription = "Bluesky Starter Packs help new users find relevant feeds and users after they joined the community."
+export const longDescription = "Bluesky Starter Packs are curated lists of users and custom feeds created by members of the community to help others navigate the platform. These packs are designed to make it easier for users to connect with specific communities or discover individuals and discussions centered around particular topics. Whether you're new to Bluesky or exploring a new area of interest, Starter Packs provide a helpful guide to finding relevant voices and engaging content, fostering stronger connections and more vibrant conversations across the network."
+
+export const links = {
+  "github": "https://github.com/Kovah/bluesky-starter-packs",
+  "kw_portfolio": "https://kovah.de",
+  "kw_bluesky": "https://kovah.de",
+  "donate": "https://buymeacoffee.com/kevinwoblick"
+}
